95846341bd5c554e6eb4020046bba047260a9f1c
Luiz Gavioli <luizbag@gmail.com>
2016-04-14 21:56:25 -0300

    Tabela de leituras está pronta

--- public/js/painel/painel_directives.js
+++ public/js/painel/painel_directives.js
@@ -1,6 +1,21 @@
 angular.module('app')
-.directive('leiturasTable', function() {
+    .directive('leiturasTable', function() {
 	return {
-		templateUrl: 'html/tabela.html'
+	    restrict: 'E',
+	    scope: {
+		leituras: '=leituras'
+	    },
+	    templateUrl: 'html/tabela.html'
 	};
-});
+    })
+    .directive('formatDate', function() {
+	return {
+	    restrict: 'E',
+	    scope: {
+		data_hora: '=data_hora'
+	    },
+	    link: function(scope, element, attrs) {
+		element.text(moment(scope.data_hora).format('DD/MM/YYYY HH:mm:ss'))
+	    }
+	};
+    });
